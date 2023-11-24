45bb4a3fd1e811921a8f5199a25768e5c64e7908
tassadit1996 <adoudou.mgh@gmail.com>
2023-11-24 17:36:48 -0300

    72

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
+import { MONGO_URI } from '../utils/variables'
 
-const URI = process.env.MONGO_URI as string
+mongoose.set("strictQuery", true)
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log('db is connected')
 
-mongoose.connect(URI).then(() => {
-    console.log('db is connected')
-
-}).catch((err) => {
-    console.log('db connection failed: ', err)
-})
+    })
+    .catch((err) => {
+        console.log('db connection failed: ', err)
+    })
