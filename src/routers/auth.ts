1369b1cbc6a8518086d69671d48bf46eccfaf1c7
tassadit1996 <adoudou.mgh@gmail.com>
2023-11-28 14:16:07 -0300

    79

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,14 +1,22 @@
 import { CreateUser } from '#/@types/user'
+import { validate } from '#/middleware/validator'
 import User from '#/models/user'
+import { CreateUserSchema } from '#/utils/validationSchema'
+
 import { Router } from 'express'
 
 const router = Router()
 
 router.post(
     "/create",
-   
+
+    validate(CreateUserSchema),
     async (req: CreateUser, res) => {
+
         const { email, password, name } = req.body
+        CreateUserSchema.validate({ email, password, name }).catch(error => {
+
+        })
         //const newUser = new User ({email, password, name})
         //newUser.save()
         const user = await User.create({ name, email, password })
--- src/utils/validationSchema.ts
+++ src/utils/validationSchema.ts
@@ -1,9 +1,21 @@
 import * as yup from 'yup'
 
 export const CreateUserSchema = yup.object().shape({
-    name: yup.string().trim().required("Name is missing!").min(3, "Name is too short!").max(20, "Name is too long!"),
-    email: yup.string().required("Email is missing!").email("Invalid email id!"),    
-    password: yup.string().trim().required("Password is missing"!).min(8, "Password is too short!").matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, "Password is too simple!")
+    name: yup
+    .string()
+    .trim()
+    .required("Name is missing!")
+    .min(3, "Name is too short!")
+    .max(20, "Name is too long!"),
+    email: yup
+    .string()
+    .required("Email is missing!")
+    .email("Invalid email id!"),    
+    password: yup
+    .string()
+    .trim()
+    .required("Password is missing"!)
+    .min(8, "Password is too short!")
+    .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, "Password is too simple!")
 })
 
-CreateUserSchema.validate()
