bfdbbae94bf61a7c4626eb738e16223d8f76f134
tassadit1996 <adoudou.mgh@gmail.com>
2023-11-29 16:38:55 -0300

    85

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -2,14 +2,22 @@ import { CreateUser } from "#/@types/user"
 import nodemailer from 'nodemailer'
 import User from "#/models/user"
 import { RequestHandler } from "express"
+import emailVerificationToken from "#/models/emailVerificationToken"
 import { GOOGLE_USER, GOOGLE_PASS } from "#/utils/variables"
+import { generateToken } from "#/utils/helpers"
 
 export const create: RequestHandler  =  async (req: CreateUser, res) => {
 
     const { email, password, name } = req.body
 
     const user = await User.create({ name, email, password })
+    
+    const token = generateToken()
+    const verificationToken = await emailVerificationToken.create({
+        owner: user._id,
+        token
 
+    })
     //send verification email
     const transport = nodemailer.createTransport({
         service: 'gmail',
@@ -22,7 +30,7 @@ export const create: RequestHandler  =  async (req: CreateUser, res) => {
       transport.sendMail({
         to: user.email,
         from: "auth@yapp.com",
-        html: "<h1>123445</h1>"
+        html: `<h1>Your verification token is ${token}</h1>`
       })
 
     res.status(201).json({ user })
