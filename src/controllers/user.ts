eb45c323ef5a17e523f4a4c9b4c97181c1659e35
tassadit1996 <adoudou.mgh@gmail.com>
2023-11-29 14:38:10 -0300

    83

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,14 +1,31 @@
 import { CreateUser } from "#/@types/user"
-
+import nodemailer from 'nodemailer'
 import User from "#/models/user"
 import { RequestHandler } from "express"
+import { MAILTRAP_PASS, MAILTRAP_USER } from "#/utils/variables"
 
 export const create: RequestHandler  =  async (req: CreateUser, res) => {
 
     const { email, password, name } = req.body
-  
 
     const user = await User.create({ name, email, password })
+
+    //send verification email
+    const transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: MAILTRAP_USER,
+          pass: MAILTRAP_PASS
+        }
+      });
+
+      transport.sendMail({
+        to: user.email,
+        from: "auth@yapp.com",
+        html: "<h1>123445</h1>"
+      })
+
     res.status(201).json({ user })
 
 }
