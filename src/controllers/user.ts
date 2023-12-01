b4a0b558082463d273b80497ddef3dda388f4e97
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-01 13:46:43 -0300

    90

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -3,6 +3,7 @@ import { CreateUser } from "#/@types/user"
 import User from "#/models/user"
 import { generateToken } from "#/utils/helpers"
 import { sendVerificationMail } from "#/utils/mail"
+import { CreateUserSchema } from "#/utils/validationSchema"
 
 import { RequestHandler } from "express"
 
@@ -12,6 +13,11 @@ export const create: RequestHandler  =  async (req: CreateUser, res) => {
     const { email, password, name } = req.body
     
     const user = await User.create({ name, email, password })
+    
+    CreateUserSchema.validate({ email, password, name }).catch(error => {
+
+    })
+
     
     // Send verification email
     const token = generateToken()
--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,6 @@
 //interface (typescript)
 
+import { compare, hash } from "bcrypt"
 import { Model, ObjectId, Schema, model } from "mongoose"
 
 interface UserDocument {
@@ -13,8 +14,11 @@ interface UserDocument {
     followers: ObjectId[]
     followings: ObjectId[]
 }
+interface Methods {
+    comparePassword(password: string): Promise<boolean>
 
-const userSchema = new Schema<UserDocument>({
+}
+const userSchema = new Schema<UserDocument, {}, Methods>({
     name:{
         type: String,
         required: true,
@@ -59,4 +63,20 @@ const userSchema = new Schema<UserDocument>({
 
 }, {timestamps: true})
 
-export default model("User", userSchema) as Model<UserDocument>
+
+userSchema.pre("save", async function(next){
+    if(this.isModified("password")){
+        this.password = await hash(this.password, 10)
+
+    }
+    next()
+})
+
+userSchema.methods.comparePassword = async function (password){
+    const result = await compare(password, this.password)
+    return result
+
+}
+    
+
+export default model("User", userSchema) as Model<UserDocument, {}, Methods>
