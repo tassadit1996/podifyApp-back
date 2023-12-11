00d8383fe2d557175876f1094ba488404879dd81
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-11 10:45:59 -0300

    103

--- src/public/script.js
+++ src/public/script.js
@@ -1,5 +1,52 @@
-const error = document.getElementById('error')
-const success = document.getElementById('success')
+const getById = (id) => {
+    return document.getElementById(id);
+}
 
-error.style.display = 'none'
-success.style.display = 'none'
+const password = getById('password');
+const confirmPassword = getById('confirm-password');
+const form = getById('form');
+const container = getById('container');
+const loader = getById('loader');
+const button = getById('submit');
+const error = getById("error");
+const success = getById("success");
+
+error.style.display = "none";
+success.style.display = "none";
+container.style.display = "none";
+
+
+let token, userId; 
+// If you plan to use URLSearchParams, you can do so without a Proxy
+window.addEventListener('DOMContentLoaded', async () => {
+    const params = new Proxy(new URLSearchParams(window.location.search), {
+        get: (searchParams, prop) => {
+            return searchParams.get(prop)
+        }
+    }) 
+    token = params.token
+    userId = params.userId
+
+    const res = await fetch("/auth/verify-pass-reset-token",
+    {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+
+        },
+        body: JSON.stringify({
+            token, userId
+        })
+    })
+
+    if(!res.ok){
+        const { error } = await res.json()
+        loader.innerText = error
+        return
+    }
+
+    loader.style.display = "none"
+    container.style.display = "block"
+  
+});
+
