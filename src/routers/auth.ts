f0676a6b1a4336e7ec24b3b7b543bbd9d4679d3a
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-13 17:43:45 -0300

    112

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -1,12 +1,9 @@
-import { CreateUser } from '#/@types/user'
+
 import { create, generateForgetPasswordLink, grantValid, sendReVerificationToken, signIn, updatePassword, verifyEmail } from '#/controllers/user'
 import { isValidPassResetToken, mustAuth } from '#/middleware/auth'
 import { validate } from '#/middleware/validator'
 import { CreateUserSchema, SignInValidationSchema, TokenAndIDValidation, updatePasswordSchema } from '#/utils/validationSchema'
-import { JWT_SECRET } from '#/utils/variables'
 import { Router } from 'express'
-import { JwtPayload, verify } from 'jsonwebtoken'
-import User from "#/models/user"
 const router = Router()
 
 router.post("/create", validate(CreateUserSchema), create)
@@ -26,18 +23,34 @@ router.get('/is-auth', mustAuth, (req, res) => {
     })
 })
 import formidable from 'formidable'
+import path from 'path'
+import fs from 'fs'
 
-router.post('/update-profile', (req, res) => {
+
+router.post('/update-profile', async (req, res) => {
 
     if(!req.headers['content-type']?.startsWith("multipart/form-data"))
         return res.status(422).json({error: "Only accepts form-data"})
 
+    const dir = path.join(__dirname, "../public/profiles")
+
+    try {
+        await fs.readdirSync(dir)
+        
+    } catch (error) {
+        await fs.mkdirSync(dir)
+    }
 
     //handle the file upload
-    const form = formidable()
+    const form = formidable({
+        uploadDir: dir,
+        filename(name, ext, part, form){
+            return Date.now() + "_" + part.originalFilename
+        },
+    })
     form.parse(req, (err, fields, files) => {
-        console.log("fields: ", fields)
-        console.log("files: ", files)
+       // console.log("fields: ", fields)
+       // console.log("files: ", files)
 
         res.json({ uploaded: true})
     })
