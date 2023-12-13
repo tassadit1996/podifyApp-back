69cb67a01a6c083bf4c00861d0e59cf886f1e4f2
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-13 16:16:14 -0300

    111

--- src/routers/auth.ts
+++ src/routers/auth.ts
@@ -25,20 +25,25 @@ router.get('/is-auth', mustAuth, (req, res) => {
 
     })
 })
+import formidable from 'formidable'
 
-router.get('/public', (req, res) => {
-    res.json({
-        message: "You are in public route."
+router.post('/update-profile', (req, res) => {
 
+    if(!req.headers['content-type']?.startsWith("multipart/form-data"))
+        return res.status(422).json({error: "Only accepts form-data"})
+
+
+    //handle the file upload
+    const form = formidable()
+    form.parse(req, (err, fields, files) => {
+        console.log("fields: ", fields)
+        console.log("files: ", files)
+
+        res.json({ uploaded: true})
     })
 })
 
-router.get('/private', mustAuth, (req, res) => {
-    res.json({
-        message: "You are in private route."
 
-    })
-})
 
 
 export default router
