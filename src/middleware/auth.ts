e8f3be1617db30dc3d92900e947b0a5d93a94da1
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-13 14:32:41 -0300

    110

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,40 +5,49 @@ import { RequestHandler } from "express";
 import { JwtPayload, verify } from "jsonwebtoken";
 
 
-
 export const isValidPassResetToken: RequestHandler = async (req, res, next) => {
-    const { token, userId} = req.body;
+    const { token, userId } = req.body;
 
-    const resetToken = await passwordResetToken.findOne({owner: userId})
-    if(!resetToken) return res.status(403).json({error: "Unauthorized access, Invalid token!"})
+    const resetToken = await passwordResetToken.findOne({ owner: userId });
+    if (!resetToken) {
+        return res.status(403).json({ error: "Unauthorized access, Invalid token!" });
+    }
 
-    const matched = await resetToken.compareToken(token)
-    if(!matched) return res.status(403).json({error: "Unauthorized access, Invalid token!"})
+    const matched = await resetToken.compareToken(token);
+    if (!matched) {
+        return res.status(403).json({ error: "Unauthorized access, Invalid token!" });
+    }
 
-    next()
+    next();
 }
 
-
-export const mustAuth: RequestHandler =  async (req, res, next) => {
-    const {authorization} = (req.headers)
-    const token = authorization?.split("Bearer ")[1]
-    if(!token) res.status(403).json({error: "Unauthorized request!" })
-
-    const payload = verify(token, JWT_SECRET) as JwtPayload
-    const id = payload.userId
-
-    const user = await User.findOne({_id: id, tokens: token})
-    if(!user) return res.status(403).json({error: "Unauthorized request!"})
-
-    req.user = {
-        id: user._id,
-        name: user.name,
-        email: user.email,
-        verified: user.verified,
-        avatar: user.avatar?.url,
-        followers: user.followers.length,
-        followings: user.followings.length
-
+export const mustAuth: RequestHandler = async (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization?.split("Bearer ")[1];
+    if (!token) {
+        return res.status(403).json({ error: "Unauthorized request!" });
     }
-    next()
+
+    try {
+        const payload = verify(token, JWT_SECRET) as JwtPayload;
+        const id = payload.userId;
+
+        const user = await User.findOne({ _id: id, tokens: token });
+        if (!user) {
+            return res.status(403).json({ error: "Unauthorized request!" });
+        }
+
+        req.user = {
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            verified: user.verified,
+            avatar: user.avatar?.url,
+            followers: user.followers.length,
+            followings: user.followings.length
+        };
+        next();
+    } catch (error) {
+        return res.status(403).json({ error: "Invalid or expired token!" });
+    }
 }
