be5b5f191e29bcad0b6847604ba8c598527105c5
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-15 17:03:37 -0300

    audioToProductTransformation

---
+++ src/models/product_category.ts
@@ -0,0 +1,24 @@
+export const productCategories = [
+    "Electronics",
+    "Fashion",
+    "Home & Garden",
+    "Sports & Outdoors",
+    "Health & Beauty",
+    "Toys & Games",
+    "Books",
+    "Automotive",
+    "Others",
+];
+
+  
+  export type productCategoryTypes =
+    | "Arts"
+    | "Business"
+    | "Education"
+    | "Entertainment"
+    | "Kids & Family"
+    | "Music"
+    | "Science"
+    | "Tech"
+    | "Others";
+  
