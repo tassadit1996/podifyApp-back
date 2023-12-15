b7f6b15f06ea715bc4c6a000783c41d8c07305c0
tassadit1996 <adoudou.mgh@gmail.com>
2023-12-15 18:48:22 -0300

    123/2

--- src/controllers/audio.ts
+++ src/controllers/audio.ts
@@ -1,5 +1,48 @@
+import { RequestWithFiles } from "#/middleware/fileParser";
+import { categoriesTypes } from "#/utils/audio_category";
 import { RequestHandler } from "express";
+import formidable from "formidable";
+import cloudinary from '#/cloud'
+import Audio from "#/models/audio";
 
-export const createAudio: RequestHandler = (req, res) => {
-    
+
+interface CreateAudioRequest extends RequestWithFiles {
+    body:{
+        title: string
+        about: string
+        category: categoriesTypes
+    }
+}
+
+export const createAudio: RequestHandler = async (req: RequestWithFiles, res) => {
+    const {title, about, category} = req.body
+    const poster = req.files?.poster as formidable.File
+    const audioFile = req.files?.files as formidable.File
+    const ownerId = req.user.id
+
+    if(!audioFile) return res.status(422).json({error: "Audio file is missing!"})
+
+    const audioRes = await cloudinary.uploader.upload(audioFile.filepath, {
+        resource_type: "video"
+    })
+
+    const newAudio = new Audio({
+        title,
+        about, 
+        category,
+        owner: ownerId,
+        file: {url: audioRes.url, publicId: audioRes.public_id}
+    })
+
+    if(poster){
+        const posterRes = await cloudinary.uploader.upload(poster.filepath, 
+        {
+            width: 300,
+            height: 300,
+            crop: "thumb",
+            gravity: "face"
+        })
+
+        newAudio.poster = {url: posterRes.secure_url, publicId: posterRes.public_id }
+    }
 }
