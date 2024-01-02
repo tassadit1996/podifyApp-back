346c7368a6b8fd0aaeb2589b86480011c0566da9
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-02 14:42:07 -0300

    146

--- src/routers/profile.ts
+++ src/routers/profile.ts
@@ -1,4 +1,4 @@
-import { getPublicUploads, getUploads, updateFollower } from "#/controllers/profile";
+import { getPublicProfile, getPublicUploads, getUploads, updateFollower } from "#/controllers/profile";
 import { mustAuth } from "#/middleware/auth";
 import { Router } from "express";
 
@@ -7,7 +7,7 @@ const router = Router()
 router.post('/update-follower/:profileId', mustAuth, updateFollower )
 
 router.get('/uploads', mustAuth, getUploads)
-
 router.get('/uploads/:profileId', getPublicUploads)
+router.get('/info/:profileId', getPublicProfile)
 
 export default router
