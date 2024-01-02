335110aa6de56a8009fe9ef83a80e2ce88918508
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-02 15:22:13 -0300

    148

---
+++ src/models/history.ts
@@ -0,0 +1,45 @@
+import { Model, Schema, model, models } from "mongoose";
+import { ObjectId } from "mongoose";
+
+type historyType = {audio: ObjectId, progress: number, date: Date}
+
+interface HistoryDocument {
+    owner: ObjectId
+    last: historyType
+    all: historyType[]
+}
+
+const historySchema = new Schema<HistoryDocument>({
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    last: {
+        audio: {type: Schema.Types.ObjectId,
+        ref: "Audio"},
+        progress: Number,
+        date: {
+            type: Date,
+            required: true
+        }
+
+    },
+    all: [
+        {
+            audio: {type: Schema.Types.ObjectId,
+            ref: "Audio"},
+            progress: Number,
+            date: {
+                type: Date,
+                required: true
+            }
+    
+        }
+    ]
+    
+}, {timestamps: true})
+
+const History = models.History || model("History", historySchema)
+
+export default History as Model<HistoryDocument>
