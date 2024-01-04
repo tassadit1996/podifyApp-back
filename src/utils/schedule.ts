c98dcdcf84db89eae6e50cf12cd25c6d9582eef3
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-04 17:06:15 -0300

    163 - Bug Solution

--- src/utils/schedule.ts
+++ src/utils/schedule.ts
@@ -11,15 +11,18 @@ import cron from 'node-cron';
 //})
 const generatedPlaylist = async () => {
     const result = await Audio.aggregate([
-        {$sort: {likes: -1}}, {$group: {
+        {$sort: {likes: -1}}, 
+
+        {
+            $sample: {size: 20},
+    
+        },
+          
+        {$group: {
             _id: "$category",
             audios: {$push: "$$ROOT._id"},
 
         }
-    },
-    {
-        $limit: 20,
-
     },
 
     ])
