01d1b2ca3c5564627566a6519e82bbc9242317a7
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-06 13:00:45 -0300

    173

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -18,6 +18,10 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
 
     const { email, password, name } = req.body
 
+
+    const oldUser = await User.findOne({email})
+    if(oldUser) return res.status(403).json({error: "Email is already in use!"})
+
     const user = await User.create({ name, email, password })
 
     CreateUserSchema.validate({ email, password, name }).catch(error => {
