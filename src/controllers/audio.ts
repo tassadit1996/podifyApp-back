cc0aba4b8b0221386b82936acaf136e0799c2312
tassadit1996 <adoudou.mgh@gmail.com>
2024-01-06 12:57:01 -0300

    172

--- src/controllers/audio.ts
+++ src/controllers/audio.ts
@@ -36,7 +36,7 @@ export const createAudio: RequestHandler = async (
         about,
         category,
         owner: ownerId,
-        file: { url: audioRes.url, publicId: audioRes.public_id },
+        file: { url: audioRes.secure_url, publicId: audioRes.public_id },
     });
 
     if (poster) {
